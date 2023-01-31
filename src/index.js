cd27d9c870bc5eba0427873e0a77684e11f4f5c6
ndj <ehdeld123@naver.com>
2023-02-01 00:27:14 +0900

    웹서버확인
    웹서버확인

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <RecoilRoot>
-      <BrowserRouter basename={process.env.NODE_ENV === "development"  ?  "/" : process.env.PUBLIC_URL }>
+      <BrowserRouter>
           <App />
       </BrowserRouter>
     </RecoilRoot>
