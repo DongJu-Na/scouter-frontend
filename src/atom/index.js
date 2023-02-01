d82d2ca17b7344185ee24a3713ad589e6acafe3e
DongJu-Na <ehdeld123@naver.com>
2023-02-01 17:03:12 +0900

    불필요 소스 제거
    불필요 소스 제거

--- src/atom/index.js
+++ src/atom/index.js
@@ -1,6 +1,4 @@
 export * from './summonerState';
-export * from './championsDataState';
-export * from './summaryDataState';
 export * from './summonerDataState';
 export * from './summonerLeagueDataState';
 export * from './matchDataState';
